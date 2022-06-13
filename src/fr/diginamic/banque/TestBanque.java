9b6b182c308abb8359ae9cc4e6efeed19222492d
Philippe MAXIMIN <philippe_maximin@yahoo.fr>
2022-06-13 15:03:11 +0200

    tp5

---
+++ src/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,16 @@
+package fr.diginamic.banque;
+
+import java.text.CompactNumberFormat;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Compte cpt = new Compte("0019101", 100);
+		
+		System.out.println(cpt);
+	}
+
+}
---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,31 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	private String numero;
+	private double solde;
+	
+	
+	public Compte(String numero, double solde) {
+		super();
+		this.numero = numero;
+		this.solde = solde;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public double getSolde() {
+		return solde;
+	}
+	
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	
+	
+}
