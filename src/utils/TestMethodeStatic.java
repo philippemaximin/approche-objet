0e69e7c0f5725d94fbe08bc23c72f2033ce49cc3
Philippe MAXIMIN <philippe_maximin@yahoo.fr>
2022-06-13 12:20:59 +0200

    tp4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String chaine= "12";
+		int intChaine = Integer.parseInt(chaine);
+		
+		int a = 5;
+		int b = 10;
+		
+		System.out.println(Integer.max(a, b));
+		
+	}
+
+}
