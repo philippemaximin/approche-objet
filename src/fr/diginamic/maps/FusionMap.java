d93160884ac7ef881befba2e08c33af9a013cb8c
Philippe Maximin <philippe_maximin@yahoo.fr>
2022-06-22 10:44:23 +0200

    TP12 - Exercice 3

---
+++ src/fr/diginamic/maps/FusionMap.java
@@ -0,0 +1,36 @@
+package fr.diginamic.maps;
+
+import java.util.HashMap;
+
+public class FusionMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Création map1
+		HashMap<Integer, String> map1 = new HashMap<Integer, String>(); 
+		map1.put(1, "Rouge");
+		map1.put(2, "Vert");
+		map1.put(3, "Orange");
+		
+		// Création map2
+		HashMap<Integer, String> map2 = new HashMap<Integer, String>(); 
+		map2.put(4, "Blanc");
+		map2.put(5, "Bleu");
+		map2.put(6, "Orange");
+		
+		//Créer une map appelée map3 et qui contient l’ensemble des données des 2 maps précédentes avec les mêmes clés et les mêmes valeurs
+		HashMap<Integer, String> map3 = new HashMap<Integer, String>();
+		
+		for (Integer key: map1.keySet()) {
+			map3.put(key, map1.get(key));
+		}
+		
+		for (Integer key: map2.keySet()) {
+			map3.put(key, map2.get(key));
+		}
+		
+		System.out.println(map3);
+	}
+
+}
